6f1ddc68a354b10abaf0ff6938c2789360490a1b
Schaefer <dennis.schaefer0612@gmail.com>
2023-01-23 14:14:21 +0100

    change the main part.

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 'use strict'
 
-console.log(document)
+
+// Function for the cookie-banner.
+    window.addEventListener("load", function(){
+    window.cookieconsent.initialise({
+        "palette": {
+            "popup": {
+                "background": "#edeff5",
+                "text": "#838391"
+            },
+            "button": {
+                "background": "#4b81e8"
+            }
+        },
+        "theme": "edgeless",
+        "content": {
+            "message": "Wir verwenden Cookies auf der Website, um Ihren Besuch effizienter zu machen und Ihnen Benutzerfreundlichkeit bieten zu können.",
+            "dismiss": "OK",
+            "link": "Informationen",
+            "href": href= "Datenschutzerklaerung.html"
+        }
+    })});
