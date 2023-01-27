c460c6f5afe5454ccc959edd040e235f840f6883
Schaefer <dennis.schaefer0612@gmail.com>
2023-01-27 17:02:17 +0100

    new design of the page.

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 'use strict'
 
 /*Häufig gestellte Fragen: question 1*/
-
 document.addEventListener('DOMContentLoaded', () => {
     const helpElements = document.querySelectorAll('[data-help]')
     for(const helpElement of helpElements){
@@ -18,9 +17,16 @@ document.addEventListener('DOMContentLoaded', () => {
     for(const cardHeader of cardHeaders){
         cardHeader.addEventListener('click', (event) => {
             const cardBody = cardHeader.nextElementSibling
-
             cardBody.classList.toggle('d-none')
+            /*just looked in the parents element 'cardHeader' */
+            const cardIcon = cardHeader.querySelector(card-toggle-icon)
 
+            /* sets the arrow down or up */
+            cardBody.classList.toggle('d-none')
+            if(cardBody.classList.contains('d-none')){
+                cardIcon.ClassList.remove('fa-angle-double-down')
+                cardIcon.ClassList.add('fa-angle-double-right')
+            }
         })
     }
 })
